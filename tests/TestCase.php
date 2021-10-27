2e5b586808daf8930dd1038455a7ba3778fe1ba7
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2021-10-27 21:21:55 +0300

    Database not required for testing and added return types

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,21 +15,20 @@ class TestCase extends Orchestra
         $this->freezeTime();
     }
 
-    protected function getPackageProviders($app)
+    protected function getPackageProviders($app): array
     {
         return [
             LinodeServiceProvider::class,
         ];
     }
 
-    public function getEnvironmentSetUp($app)
+    public function getEnvironmentSetUp($app): void
     {
-        config()->set('database.default', 'testing');
         config()->set('linode.environment', 'testing');
         config()->set('linode.token', '644e87didyousurelyexpectmetocommittheactualkey2680ea1d15b8c5f122');
     }
 
-    private function freezeTime()
+    private function freezeTime(): void
     {
         Carbon::setTestNow('2018-01-01T00:01:01');
     }
