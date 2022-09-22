a0e1721bc25f29795d3e23b655b9349031d7478b
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-09-22 18:03:12 +0300

    imports

--- tests/AccountTest.php
+++ tests/AccountTest.php
@@ -1,15 +1,18 @@
 <?php
 
+use SamuelMwangiW\Linode\DTO\AccountDTO;
+use SamuelMwangiW\Linode\Exceptions\CredentialsMissing;
 use SamuelMwangiW\Linode\Linode;
+use SamuelMwangiW\Linode\Request\Account\GetRequest;
 
 it('authenticate throws an exception when linode.config is not set', function () {
     config()->set('linode.token', '');
-    \SamuelMwangiW\Linode\Request\Account\GetRequest::build()->fetch();
-})->throws(\SamuelMwangiW\Linode\Exceptions\CredentialsMissing::class);
+    GetRequest::build()->fetch();
+})->throws(CredentialsMissing::class);
 
 it('linode returns an account')
     ->expect(fn () => Linode::account())
-        ->toBeInstanceOf(\SamuelMwangiW\Linode\DTO\AccountDTO::class)
+        ->toBeInstanceOf(AccountDTO::class)
         ->company->toBe('mwangithegreat')
         ->email->toBe('samuel@samuelmwangi.co.ke')
         ->uid->toBe('D09103B1-20FE-11EA-B88C0CC47AEB2714');
