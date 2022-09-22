7473fc0ea08de79f76a37033a18cd5e398f0fd5a
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-09-22 18:54:49 +0300

    Install saloon

--- config/linode.php
+++ config/linode.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-// config for SamuelMwangiW/LinodeCli
 return [
     'endpoint' => env('LINODE_API_ENDPOINT', 'https://api.linode.com/v4/'),
     'token' => env('LINODE_PERSONAL_ACCESS_TOKEN'),
