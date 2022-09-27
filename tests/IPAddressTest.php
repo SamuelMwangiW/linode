619d1e4815a71ed4a789785f4be4b5eb1eccbdb1
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-09-27 09:08:32 +0300

    WIP

---
+++ tests/IPAddressTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use SamuelMwangiW\Linode\DTO\IPAddress;
+
+it('returns true for ipv4 addresses', function (string $ip) {
+    $IpAddressObj = new IPAddress($ip);
+
+    expect($IpAddressObj)
+        ->ip->toBe($ip)
+        ->isV4()->toBeTrue()
+        ->isV6()->toBeFalse();
+})->with('valid-ipv4');
+
+it('returns true for private ipv4 addresses', function (string $ip) {
+    $IpAddressObj = new IPAddress($ip);
+
+    expect($IpAddressObj)
+        ->ip->toBe($ip)
+        ->isV4()->toBeTrue()
+        ->isPrivate()->toBeTrue()
+        ->isPublic()->toBeFalse();
+})->with('private-ipv4');
+
+it('returns true for public ipv4 addresses', function (string $ip) {
+    $IpAddressObj = new IPAddress($ip);
+
+    expect($IpAddressObj)
+        ->ip->toBe($ip)
+        ->isV4()->toBeTrue()
+        ->isPublic()->toBeTrue()
+        ->isPrivate()->toBeFalse();
+})->with('public-ipv4');
